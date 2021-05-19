833281f9799426f850276f44b761866e006fb732
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-19 21:30:17 +0000

    html with js template string

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,5 +2,81 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+*/
 
+
+$(document).ready(function() {
+  
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1621224103078
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1621310503078
+    }
+  ];
+
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`<article class="single-tweet">
+    <div class="box-top">
+      <div class="img">
+        <img id="img-size" src="${tweet.user.avatars}" />
+        <tag>${tweet.user.name}</tag> 
+      </div>
+        <span id="handler">${tweet.user.handle}</span>
+    </div>
+    <p>${tweet.content.text}</p>
+    <hr class="underline"/>
+    <div class="bottom-container">
+      <span class="timeline">
+        ${timeago.format(tweet.created_at)}
+      </span>
+      <div>
+        <i class="fas fa-flag status"></i> 
+        <i class="fas fa-retweet status"></i>
+        <i class="fas fa-heart status"></i>
+      </div>
+    </div>
+  </article>`);
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    tweets.forEach((tweet) => {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    });
+  };
+  
+  renderTweets(tweetData);
+
+  $('#form-submit').submit(function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    // XMLHttpRequest();
+  });
+});
+
+
+$('.timeline').html(timeago.format(new Date()));
+console.log(timeago.format(new Date()));
