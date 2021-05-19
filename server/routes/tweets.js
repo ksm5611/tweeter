0e29c724ca535d8c386f665d80333a994559dbeb
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-19 21:47:40 +0000

    fixing linting problem

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -3,7 +3,6 @@
 const userHelper    = require("../lib/util/user-helper");
 
 const express       = require('express');
-const bodyParser = require("body-parser");
 const tweetsRoutes  = express.Router();
 
 module.exports = function(DataHelpers) {
@@ -18,7 +17,7 @@ module.exports = function(DataHelpers) {
     });
   });
 
-  tweetsRoutes.post("/", bodyParser.json(), function(req, res) {
+  tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
